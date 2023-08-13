df21bf052d04c4edce88b9d3fcc749b7866fd638
misikira <misikirteka@gmail.com>
2023-08-13 09:50:32 +0300

    more components added

--- pizza-menu/src/App.js
+++ pizza-menu/src/App.js
@@ -6,11 +6,9 @@ function App() {
   return (
     <div>
     <h1>react app</h1>
-    <Pizza/>
-    <Pizza/>
-    <Pizza/>
-    <Pizza/>
-    <Pizza/>
+    <Header/>
+    <Menu/>
+    <Footer/>
     </div>
   );
 }
@@ -21,6 +19,29 @@ function Pizza (){
     <h1>Pizza Salamino</h1>
     <p>Tomato, mozarella, and pepperoni</p>
   </div>
+  )
+}
+function Header(){
+  return(
+    <h1>Fusion Bites Pizza Co.</h1>
+  )
+}
+function Footer(){
+  return(
+    <footer>{new Date().toLocaleTimeString()}we are currently open</footer>
+  )
+}
+
+function Menu(){
+  return (
+    <div>
+    <h2>our menu</h2>
+    <Pizza/>
+    <Pizza/>
+    <Pizza/>
+    <Pizza/>
+    <Pizza/>
+    </div>
   )
 }
 export default App;
