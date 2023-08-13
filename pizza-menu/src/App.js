98e8f6bfac0cfad87dddccb6316bc8420978d342
misikira <misikirteka@gmail.com>
2023-08-13 16:31:12 +0300

    add the render list feature of react

--- pizza-menu/src/App.js
+++ pizza-menu/src/App.js
@@ -1,11 +1,55 @@
 
 import logo from './logo.svg';
 import './App.css';
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
 
 function App() {
   return (
     <div className='container'>
-    <h1>react app</h1>
     <Header/>
     <Menu/>
     <Footer/>
@@ -38,35 +82,32 @@ function Menu(){
   return (
     <main className='menu'>
     <h2>our menu</h2>
-    <Pizza 
+    <ul className='pizzas'>
+      {pizzaData.map((pizza)=>(
+        <Pizza Pizzaobj={pizza} key={pizza.name}/>
+      ))}
+    </ul>
+    {/* <Pizza 
     name="Pizza Salamino"
     ingredeints="Tomato, mozarella, and pepperoni"
     photoname='pizzas/prosciutto.jpg'
-    price='10'
-    />
-
-<Pizza 
-    name="Pizza Fungi"
-    ingredeints="Tomato, mozarella, and pepperoni"
-    photoname='pizzas/funghi.jpg'
-    price='15'
-    />
-    
-    </main >
+    price={10}
+    /> */}
+ </main >
   )
 }
 
 function Pizza (props){
   return(
-    <div className="pizza"> 
-    <img src={props.photoname} alt={props.name}/>
+    <li className="pizza"> 
+    <img src={props.Pizzaobj.photoName} alt={props.Pizzaobj.name}/>
     <div>
-     <h1>{props.name}</h1>
-    <p>{props.ingredeints}</p>
-    <span>{props.price}</span>
+     <h1>{props.Pizzaobj.name}</h1>
+    <p>{props.Pizzaobj.ingredients}</p>
+    <span>{props.Pizzaobj.price}</span>
     </div>
-
-  </div>
+ 
+  </li>
   )
 }
 export default App;
