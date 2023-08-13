cce3a4fe5d6311b57e9624ea20a75585d3b23900
misikira <misikirteka@gmail.com>
2023-08-13 09:11:41 +0300

    add pizza component

--- pizza-menu/index.js
+++ pizza-menu/index.js
@@ -1,2 +1,6 @@
 import React from "react";
 import { ReactDOM } from "react";
+function App(){
+    return ( <h1>hello react</h1>
+    )
+}
--- pizza-menu/src/App.js
+++ pizza-menu/src/App.js
@@ -1,10 +1,18 @@
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <div>
     <h1>react app</h1>
+    <Pizza/>
+    </div>
   );
 }
-
+function Pizza (){
+  return(
+    <h1>pizaa fusion</h1>
+  )
+}
 export default App;
